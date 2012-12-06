04a1279ce96dda543938df0dbb16e64846d8f2e5
craigwillard <cwillard@tammantech.com>
2012-12-06 16:54:13 -0500

    user scaffold added

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  attr_accessible :admin, :email, :name, :password, :password_confirmation, :username
+
+  has_secure_password
+  validates_presence_of :password, :on => :create
+end
